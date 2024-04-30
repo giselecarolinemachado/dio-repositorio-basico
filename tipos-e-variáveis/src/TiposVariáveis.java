4d9cc62d586c9cef968987db9844fcfd3761aaea
Gisele <103590517+giselecarolinemachado@users.noreply.github.com>
2024-04-30 16:02:39 -0300

    Delete tipos-e-variáveis directory
    Estou excluindo pois criei dois arquivos para o desafio da conta bancária.

--- tipos-e-variáveis/src/TiposVariáveis.java
+++
@@ -1,7 +0,0 @@
-public class TiposVariáveis {
-       public static void main(String[] arg) throws Exception {
-        final double VALOR_DE_PI = 18;
-        String nomeCompleto = "Linguagem " + "Java";
-        System.out.println(nomeCompleto);
-    }
-}
